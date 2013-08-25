37c8dd2528408e7d9c8e4ffeb6c0f6f0cdf48f17
Nana Ewusi <precious.ewusi.nyarko@meltwater.org>
2013-08-25 16:52:48 +0000

    Branded the default express greeting for DevCongress

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@
 exports.init = function(app) {
 	// Setup index route
 	app.get('/', function(req, res){
-		res.render('index', { title: 'Express' });
+		res.render('index', { title: 'DevCongress Jobs' });
 	});
 }
